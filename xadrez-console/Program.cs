011e7f9430a7c2f0ed573dc333871b0c2f9507bc
Tauan Maciel <tauan.freitas1112@gmail.com>
2025-02-25 11:03:36 -0300

    first commit

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xadrez_console.Tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(1, 2);
+            Console.WriteLine(P);
+        }
+    }
+}
---
+++ xadrez-console/Tabuleiro/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Vermelha,
+        Azul,
+        Verde,
+        laranja
+    }
+}
